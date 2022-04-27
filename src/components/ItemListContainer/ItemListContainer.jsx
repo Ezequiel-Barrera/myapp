442e62b4d0596ddc19e2d2f6429d4e9143e6bd31
Ezequiel Andres Barrera <ezequiel.abarrera@gmail.com>
2022-04-26 22:53:20 -0300

    utilizo react-bootstrap para crear el boton de contar y cambiar el menu

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,10 @@
+import ItemCount from "../ItemCount/ItemCount"
 
 const ItemListContainer = (greeting) => {
   return (
     <>
       {greeting.text}
+      <ItemCount stock={10} initial={1}/>
     </>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,30 @@
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import CartWidget from '../CartWidget/CartWidget'
 import './NavBar.css'
 
 
 const NavBar = () => {
     return (
-        <nav className='nav'>
-            <label>Quick-miniBuy Store</label>
-            <ul className='nava'>
-                <li><a href='#'>Tortas Caseras</a></li>
-                <li><a href='#'>Sandwich de Miga</a></li>
-                <li><a href='#'>Alfajores y Conitos</a></li>
-            </ul>
+        <Navbar bg="light" expand="lg">
+        <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">About us</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+            </Nav>
+            </Navbar.Collapse>
             <CartWidget />
-        </nav>
+        </Container>
+        </Navbar>
     )
 }
 
