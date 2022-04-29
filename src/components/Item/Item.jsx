c0dabd8ca14a41fa4fde2324314caffb93ec9e07
Ezequiel Andres Barrera <ezequiel.abarrera@gmail.com>
2022-04-28 21:21:08 -0300

    agrego los componentes de Item e ItemList, y desde un array de objetos trango para producto

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,10 @@
+const Item = ({ title, price }) => {
+    return (
+    <>
+        <h1>{title}</h1>
+        <h2>${price}</h2>
+    </>
+    );
+};
+
+export default Item;
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,24 @@
 import ItemCount from "../ItemCount/ItemCount"
+import ItemList from "../ItemList/ItemList"
+import getData from "../../services/getData"
+import { useState, useEffect } from "react"
 
 const ItemListContainer = (greeting) => {
+
+  const [products, setProducts] = useState([])
+  console.log("Products before promise", products);
+
+  useEffect(() => {
+    getData
+      .then((response) => setProducts(response))
+      .catch((error) => console.log("error: ", error));
+  }, [])
+
   return (
     <>
       {greeting.text}
-      <ItemCount stock={10} initial={1}/>
+      <ItemCount stock={10} initial={1} />
+      <ItemList products={products} />
     </>
   )
 }
